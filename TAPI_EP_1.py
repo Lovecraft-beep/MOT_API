9edad6e150ced6082184df32e40b7742414d5ba8
Lovecraft-beep <77508809+Lovecraft-beep@users.noreply.github.com>
2021-01-18 14:30:34 +0000

    Update TAPI_EP_1.py

--- TAPI_EP_1.py
+++ TAPI_EP_1.py
@@ -1,4 +1,4 @@
 # Basic Python Script for TAPI query (Endpoint 1)
 import requests
-response = requests.get("https://beta.check-mot.gov.uk/trade/vehicles/mot-tests?registration={S872CHU}"")
+response = requests.get("https://beta.check-mot.gov.uk/trade/vehicles/mot-tests?registration={S872CHU}")
 print(response.status_code)
