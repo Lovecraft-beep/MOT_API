a5cee8bb3b61589809cd28dca941adcb624fe08d
Lovecraft-beep <77508809+Lovecraft-beep@users.noreply.github.com>
2021-01-28 14:57:12 +0000

    Delete TAPI_EP_1.py

--- TAPI_EP_1.py
+++
@@ -1,4 +0,0 @@
-# Basic Python Script for TAPI query (Endpoint 1)
-import requests
-response = requests.get("https://beta.check-mot.gov.uk/trade/vehicles/mot-tests?registration={S872CHU}")
-print(response.status_code)
