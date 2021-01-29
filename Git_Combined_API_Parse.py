819f2ca6683598865be8e029b7c6d1f6c4aa94ed
Lovecraft-beep <77508809+Lovecraft-beep@users.noreply.github.com>
2021-01-29 11:17:04 +0000

    Add files via upload

--- Git_Combined_API_Parse.py
+++ Git_Combined_API_Parse.py
@@ -1,10 +1,13 @@
 import requests
 import csv
 import json
+from datetime import datetime
+
+start = datetime.now()
 
 headers = {
 	'Accept': 'application/json+v6',
-	'x-api-key': 'xxx-xxxx-xxx-xxxx-xxx',
+	'x-api-key': 'api-key-here',
 }
 
 with open('vrms.csv', newline='') as csvfile:
@@ -18,53 +21,72 @@ with open('vrms.csv', newline='') as csvfile:
 
         response = requests.get('https://beta.check-mot.service.gov.uk/trade/vehicles/mot-tests/', headers=headers, params=params)
 
-        car_string = response.text
-
-        car_dict = json.loads(car_string)
-
-        with open('s872chu.json', 'w') as json_file:
-            json.dump(car_dict, json_file, skipkeys = False, sort_keys = False, separators = (',', ':'), indent = 3)
-
-        with open('s872chu.json', 'r') as f: 
-            vehicle_dict = json.load(f)
- 
-        for vehicle in vehicle_dict: 
-
-            vrm = vehicle['registration']
-            make = vehicle['make']
-            model = vehicle['model']
-            colour = vehicle['primaryColour']
-            fuel = vehicle['fuelType']
-            engsize =  vehicle['engineSize']
-            registered = vehicle['registrationDate']
-
-            print(vrm)
-            print(make)
-            print(model)
-            print(colour)
-            print(fuel)
-            print(engsize)
-            print(registered)
-
-            mots = vehicle['motTests']
+        if response.status_code == 404:
             
-            for test_data in mots:
-                result = test_data['testResult']
-                testDate = test_data['completedDate']
-                odovalue = test_data['odometerValue']
+            ouch = "VRM " + vrm +" is not valid"
+            print(ouch)
+            car_string = json.dumps(ouch)
+
+        else:   
+            car_string = response.textcar_string = response.text
+
+            car_dict = json.loads(car_string)
+
+            with open('temp.json', 'w') as json_file:
+                json.dump(car_dict, json_file, skipkeys = False, sort_keys = False, separators = (',', ':'), indent = 3)
+
+            with open('temp.json', 'r') as f: 
+                vehicle_dict = json.load(f)
+     
+            for vehicle in vehicle_dict: 
+
+                reg = vehicle['registration']
+                make = vehicle['make']
+                model = vehicle['model']
+                colour = vehicle['primaryColour']
+                fuel = vehicle['fuelType']
+                
                 try:
-                    expiry = test_data['expiryDate']
-                except:
-                    expiry = "N/A"
+                    engsize =  vehicle['engineSize']
+                except KeyError:
+                    engsize = 'Not Available'
                     
-                print(testDate, result, odovalue, expiry)
+                registered = vehicle['registrationDate']
 
-                comments = test_data['rfrAndComments']
+                print(reg)
+                print(make)
+                print(model)
+                print(colour)
+                print(fuel)
+                print(engsize)
+                print(registered)
+            
+                try:
+                    mots = vehicle['motTests']
+                
+                    for test_data in mots:
+                        result = test_data['testResult']
+                        testDate = test_data['completedDate']
+                        odovalue = test_data['odometerValue']
+                        try:
+                            expiry = test_data['expiryDate']
+                        except:
+                            expiry = "N/A"
+                            
+                        print(testDate, result, odovalue, expiry)
 
-                for rfr in comments:
-                    text = rfr['text']
-                    ftype = rfr['type']
-                    dang = rfr['dangerous']
+                        comments = test_data['rfrAndComments']
 
-                    print(text,ftype,dang)
- 
+                        for rfr in comments:
+                            text = rfr['text']
+                            ftype = rfr['type']
+                            dang = rfr['dangerous']
+
+                            print(text,ftype,dang)
+         
+                except KeyError:
+                    print('No Test Data Available')
+                    
+end = datetime.now()
+time_taken = end - start
+print('Time: ',time_taken)
